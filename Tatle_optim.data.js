f465d3c10a95333774d7f9ede5e858a802594eff
AtomDixon <88931319+AtomDixon@users.noreply.github.com>
2021-08-14 19:23:46 +0300

    Update Tatle_optim.data.js

--- Tatle_optim.data.js
+++ Tatle_optim.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/l693dfen0nwxe3h/Tatle_optim.data?dl=0';
-    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/l693dfen0nwxe3h/Tatle_optim.data?dl=0';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/frlru79sav7jz5i/Tatle_optim.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/frlru79sav7jz5i/Tatle_optim.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');

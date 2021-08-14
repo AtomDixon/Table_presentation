726f9027efbb6f70f91f4dfe6f6293e6dd4e4a26
AtomDixon <88931319+AtomDixon@users.noreply.github.com>
2021-08-14 18:22:05 +0300

    Update Tatle_optim.UE4.js

--- Tatle_optim.UE4.js
+++ Tatle_optim.UE4.js
@@ -1154,7 +1154,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Tatle_optim.data.js'));
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/AtomDixon/Table_presentation/7eda9db924ba78efe1048e255efcec29490fa569/Tatle_optim.data.js'));
 		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
